7a08bebbce963229a6b8172b95e69b16e5714a21
Phill Legnitto <palegnitto@gmail.com>
2022-07-15 16:50:01 -0700

    add two-array-object

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,27 @@
-// add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+ /*
+
+
+
+ conditions:
+ - not enough values  keys should have value of null
+ - not enough keys, don't worry about it
+
+ return:
+ object with key / value pairs.
+
+ will need th iterator variable of a standard for loop to 
+ handle referencing the other arrya without actually interacting with it.
+
+ check if there's a value and if not set it to null
+
+ */
+function twoArrayObject(arrKeys,arrVals) {
+
+    let obj = {};
+
+    for (let i = 0; i < arrKeys.length; i++){
+        (arrVals[i]) ? obj[arrKeys[i]] = arrVals[i] :  obj[arrKeys[i]] = null;
+    }
+
+    return obj;
 }
