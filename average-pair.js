3e938536ccd2071d2b6de7ffcdb5ea1bc3ee08f3
Emily Chen <sticky_sugar@hotmail.com>
2022-07-15 16:35:00 -0700

    function for average-pairs

--- average-pair.js
+++ average-pair.js
@@ -1,5 +1,32 @@
+// find the average of pairs from the array
+//compare against the target
+//return true if a pair's average is equal to target
 
+function averagePair(nums, targetAvg) {
+  let left = 0;
+  let right = nums.length - 1;
 
-function averagePair() {
+  //find the average of the each pair the pointers are pointing at
+  //we'll check it against the target average
+  //if the average is less than the target average,
+  //then we'll increase the left pointer
+  //otherwise we will decrease the left pointer
 
+  while (left < right) {
+    let avg = (nums[left] + nums[right]) / 2;
+
+    if (avg < targetAvg) {
+      left++;
+    }
+
+    if (avg > targetAvg) {
+      right--;
+    }
+
+    if (avg === targetAvg) {
+      return true;
+    }
+  }
+
+  return false;
 }
